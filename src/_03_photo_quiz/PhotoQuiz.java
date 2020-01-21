f90dc37378a4606be5c0db9c078da4325d60cf35
League Student <league@iMac-23.attlocal.net>
2020-01-20 17:00:16 -0800

    PhotoQuiz, SpinningRecord, RainGame, MagicWorms, DrumKit(IP)

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -26,35 +26,45 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-
+String imageAdress1 = "https://imgix.ranker.com/user_node_img/50026/1000504352/original/high-five-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces;";
 		// 2. create a variable of type "Component" that will hold your image
-
+Component picture1;
 		// 3. use the "createImage()" method below to initialize your Component
-
+picture1=createImage(imageAdress1);
 		// 4. add the image to the quiz window
-
+quizWindow.add(picture1);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String ans1 = JOptionPane.showInputDialog("What kind of dog is this?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if(ans1.equalsIgnoreCase("pomeranian")) {
+	JOptionPane.showMessageDialog(null, "CORRECT");
+}else {
+	JOptionPane.showMessageDialog(null, "INCORRECT");
+}
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+quizWindow.remove(picture1);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+String imageAdress2 = "https://images-na.ssl-images-amazon.com/images/I/510K%2BmJcGML._AC_SX425_.jpg";
 		// 11. add the second image to the quiz window
-
+Component picture2 = createImage(imageAdress2);
+quizWindow.add(picture2);
 		// 12. pack the quiz window
-
+quizWindow.pack();
 		// 13. ask another question
 
+String ans2 = JOptionPane.showInputDialog("What kind of dog is this?");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+if(ans2.equalsIgnoreCase("pug")) {
+	JOptionPane.showMessageDialog(null, "CORRECT");
+}else {
+	JOptionPane.showMessageDialog(null, "INCORRECT");
+}
 	}
 
 	private Component createImage(String imageUrl) throws MalformedURLException {
